ec2bec06d3f1b3e7d6f1ca51bce7b8d8d07bdf32
Chris Kiriakou <chris-kiriakou@web.de>
2025-07-27 10:55:58 +0200

    Remove wpctl readout

--- scripts/.local/bin/status.sh
+++ scripts/.local/bin/status.sh
@@ -6,7 +6,7 @@ get_battery() {
     read bat_perc < /sys/class/power_supply/BAT1/capacity
     read bat_status < /sys/class/power_supply/BAT1/status
     # Send warning if battery is low and no charger is plugged in
-    test $bat_perc -le 5 && test $bat_status != 'Charging' \
+    test "${bat_perc}" -le 5 && test "${bat_status}" != 'Charging' \
         && notify-send --urgency=ciritcal "Plug in a charger, low battery!"
     printf "${bat_perc}-${bat_status}"
 }
@@ -14,9 +14,7 @@ get_battery() {
 main() {
     test -d /sys/class/power_supply/BAT1 && bat=$(get_battery)
     my_date=$(date "+%a,%d.%m.%y-%H:%M")
-    read _ vol <<< $(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-    vol="vol:${vol}"
-    printf "${vol}  ${bat}  ${my_date}\n"
+    printf "${bat}  ${my_date}  \n"
 }
 
 main
