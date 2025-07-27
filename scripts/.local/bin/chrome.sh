0770eb059f7c79389c9b66a6b96cde0697a8f3f3
Chris Kiriakou <chris-kiriakou@web.de>
2025-07-27 11:22:27 +0200

    Remove unused scripts

--- scripts/.local/bin/chrome.sh
+++
@@ -1,7 +0,0 @@
-#/bin/sh
-#
-# Start the chromium webbrowser with wayland flags for right scaling
-
-chromium \
-    --enable-features=UseOzonePlatform \
-    --ozone-platform=wayland
--- scripts/.local/bin/code.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# Start VS Code with required flags under wayland
-
-/opt/VSCode-linux-x64/bin/code --ozone-platform=wayland
