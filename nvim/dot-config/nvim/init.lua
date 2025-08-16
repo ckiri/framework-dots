1464d879b0fc53d1b87af4e8f0ec96bf4c750483
Chris Kiriakou <chris-kiriakou@web.de>
2025-08-16 16:00:24 +0200

    change indentation, Use nvim native package manager.

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -8,7 +8,7 @@ vim.g.nwtrw_liststyle = 3 vim.g.netrw_banner = 0
 
 -- Options
 vim.opt.termguicolors = false
-vim.opt.laststatus = 2
+vim.opt.laststatus = 1
 vim.opt.mouse = "a"
 vim.opt.showmode = true
 vim.opt.clipboard = "unnamedplus"
@@ -23,9 +23,9 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.inccommand = "split"
 vim.opt.scrolloff = 10
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftround = false
 vim.opt.hidden = true
 vim.opt.showcmd = true
@@ -41,49 +41,44 @@ vim.opt.number = false
 vim.opt.relativenumber = false
 vim.opt.smartindent = true
 vim.opt.autoindent = true
---vim.opt.colorcolumn = { "80", "120" }
+vim.opt.colorcolumn = { "80" }
 
 -- Legacy vim options, because I can't figure out how to set them in lua
 vim.cmd("set path+=**")
 vim.cmd("set guicursor=")
 
+vim.pack.add({
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/vimwiki/vimwiki" },
+})
 -- Vimwiki configuration -------------------------------------------------------
 
 vim.g.vimwiki_list = {
-    {
-        path = "~/docs/vimwiki",
-        path_html = "~/docs/vimwiki-html",
-        template_path = "~/docs/vimwiki/template",
-        template_default = "def_template",
-        template_ext = ".html"
-    },
-    {
-        path = "~/docs/homewiki",
-        path_html = "~/docs/homewiki-html"
-    }
+  {
+    path = "~/docs/vimwiki",
+    path_html = "~/docs/vimwiki-html",
+    template_path = "~/docs/vimwiki/template",
+    template_default = "def_template",
+    template_ext = ".html",
+  },
+  {
+    path = "~/docs/knet/wiki",
+    syntax = 'markdown',
+    ext = '.md',
+  }
 }
 
 -- Completion setup -----------------------------------------------------------
 
-local cmp = require("cmp")
-cmp.setup({
-    window = {
-        completion = cmp.config.window.bordered({border = "single" }),
-        documentation = cmp.config.window.bordered({border = "single" }),
-    },
-    mapping = cmp.mapping.preset.insert({
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-Space>"] = cmp.mapping.complete({}),
-    }),
-    sources = {
-        { name = "nvim_lsp" },
-    }
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client:supports_method('textDocument/completion') then
+      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+    end
+  end,
 })
-
--- Add LSP capabilities to cmp
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+vim.cmd("set completeopt+=noselect")
 
 -- Key mappings ---------------------------------------------------------------
 
@@ -95,19 +90,19 @@ vim.keymap.set("n", "<C-j>", ":bnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-k>", ":bprev<CR>", { noremap = true, silent = true })
 
 local on_attach = function(_, bufnr)
-    local opts = { buffer = bufnr, silent = true }
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "gh", vim.lsp.buf.signature_help, opts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+  local opts = { buffer = bufnr, silent = true }
+  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+  vim.keymap.set("n", "gh", vim.lsp.buf.signature_help, opts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 end
 
 -- Language server setup -------------------------------------------------------
@@ -124,13 +119,10 @@ lspconfig.clangd.setup({
 -- Python
 lspconfig.pylsp.setup{}
 
+-- Lua
+vim.lsp.enable('lua_ls')
 
 -- UI & colors ----------------------------------------------------------------
 
--- Override default window options for consistent border drawing
-local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
-function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
-    opts = opts or {}
-    opts.border = opts.border or "single"
-    return orig_util_open_floating_preview(contents, syntax, opts, ...)
-end
+-- Borders
+vim.opt.winborder = "single"
